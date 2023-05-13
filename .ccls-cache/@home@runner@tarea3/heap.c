de733c3e4e2db3f35917b23b522dc4742da22b49
Ratinaxo <108155134+Ratinaxo@users.noreply.github.com>
2023-05-13 01:00:53 +0000

    Importadas funciones de priority queue

---
+++ .ccls-cache/@home@runner@tarea3/heap.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include "heap.h"
+
+typedef struct nodo{
+   void* data;
+   int priority;
+}heapElem;
+
+typedef struct Heap{
+  heapElem* heapArray;
+  int size;
+  int capac;
+} Heap;
+
+void swap(heapElem *a, heapElem *b){
+  heapElem aux = *a;
+  *a = *b;
+  *b = aux;
+  return;
+}
+
+void* heap_top(Heap* pq){
+  if (pq == NULL || pq->size == 0)return NULL;
+  return pq->heapArray[0].data;
+}
+
+void heap_push(Heap* pq, void* data, int priority){
+  if (pq == NULL || pq->size == 0)return;
+  if(pq->size == pq->capac){
+    int newCapac = (pq->capac * 2) + 1;
+    pq->heapArray = realloc(pq->heapArray, (newCapac * sizeof(heapElem)));
+    if(pq->heapArray == NULL)return;
+    pq->capac = newCapac;
+  }
+
+  int index = pq->size;
+  pq->heapArray[index].data = data;
+  pq->heapArray[index].priority = priority;
+  pq->size++;
+
+  while(index > 0 && pq->heapArray[(index - 1) / 2].priority < pq->heapArray[index].priority){
+    swap(&pq->heapArray[(index - 1) / 2], &pq->heapArray[index]);
+    index = (index - 1) / 2;
+  }
+  return;
+}
+
+
+void heap_pop(Heap* pq){
+  if(pq == NULL || pq->size == 0)return;
+  pq->heapArray[0] = pq->heapArray[pq->size - 1];
+  pq->size--;
+
+  int index = 0;
+  while(1){
+    int izq = (index * 2) + 1;
+    int der = (index * 2) + 2;
+    int grande = index;
+
+    if (izq < pq->size && pq->heapArray[izq].priority > pq->heapArray[grande].priority)
+      grande = izq;
+    
+    if (der < pq->size && pq->heapArray[der].priority > pq->heapArray[grande].priority)
+      grande = der;
+
+    if(grande == index)break;
+
+    swap(&pq->heapArray[index], &pq->heapArray[grande]);
+    index = grande;
+  }
+  return;
+}
+
+Heap* createHeap(){
+  Heap *heap = (Heap*)malloc(sizeof(Heap));
+  if (heap == NULL)return NULL;
+
+  heap->heapArray = (heapElem*)malloc(3 * sizeof(heapElem));
+  if (heap->heapArray == NULL){
+    free(heap);
+    return NULL;
+  }
+
+  heap->size = 0;
+  heap->capac = 3;
+  return heap;
+}
--- .ccls-cache/@home@runner@tarea3/main.c
+++ .ccls-cache/@home@runner@tarea3/main.c
@@ -1,5 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "heap.h"
+
+typedef struct{
+  char *nombre;
+  int prioridad;
+}tarea;
+
+typedef struct{
+  tarea *tareas;
+  int size;
+  int capac;
+}colaTareas;
+
+
+void agregarTarea(char *nombreTarea, int prioridad){
+  
+}
+
+void precedencia(char *tarea1, char *tarea2){
+  
+}
+
+void mostrarTareas(colaTareas listaTareas){
+  
+}
 
 int main(){
-
+  colaTareas *listaTareas = (colaTareas*)createHeap();
+  if (listaTareas == NULL){
+    printf("ERROR DE MEMORIA");
+    exit(EXIT_FAILURE);
+  }
 }
